b866e38866f4d2219b1e8f9a8172de84ab5999c4
Alexander Merritt <merritt.alex@gatech.edu>
2014-12-18 01:22:44 -0500

    opencv- small modifications to jar script

--- src/opencv/storm/compile_java.sh
+++ src/opencv/storm/compile_java.sh
@@ -2,23 +2,32 @@
 set -e
 set -u
 
+source /etc/profile.d/java.sh
+
 # add others as necessary, colon-separated
-JARS=/home/merrital/local/storm/lib/storm-core-0.9.1-incubating.jar
+JARS=/usr/local/src/storm/latest/lib/storm-core-0.9.3.jar
+
+[[ ! -e "$JARS" ]] && echo "storm jar not found" && exit 1
 
 killall -s 1 stormstitcher || true
 
 echo Compiling ...
 javac -cp $JARS StitcherTopology.java
 
-[ ! -e ../stormstitcher ] && \
+[[ ! -e ../stormstitcher ]] && \
 	echo "Compile stormstitcher first" && exit 1
-[ ! -e ../pulse.conf ] && \
+
+[[ ! -e ../pulse.conf ]] && \
 	echo "../pulse.conf missing" && exit 1
-[ ! -d resources ] && mkdir resources
+
+[[ ! -d resources ]] && mkdir resources
+
 cp ../stormstitcher resources/
 cp ../pulse.conf resources/
 
 echo Creating jar ...
 jar cf stitcher.jar Stitcher*.class resources/
+
 rm -f *.class
 echo Done.
+
