24f62d1c502f8483df243e838299b1145332ad49
Alexander Merritt <merritt.alex@gatech.edu>
2014-12-19 00:36:59 -0500

    incremental changes to the storm workflow

--- src/opencv/storm/SearchTopology.java
+++ src/opencv/storm/SearchTopology.java
@@ -1,200 +1,160 @@
-import backtype.storm.Config;
-import backtype.storm.LocalCluster;
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.spout.ShellSpout;
-import backtype.storm.StormSubmitter;
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.ShellBolt;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.coordination.IBatchBolt;
-import backtype.storm.coordination.BatchOutputCollector;
-import backtype.storm.topology.base.BaseBasicBolt;
-import backtype.storm.topology.base.BaseRichSpout;
-import backtype.storm.topology.IRichBolt;
-import backtype.storm.topology.IRichSpout;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.topology.BasicOutputCollector;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
-import java.lang.Integer;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.lang.IllegalArgumentException;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import backtype.storm.*;
+import backtype.storm.coordination.*;
+import backtype.storm.drpc.*;
+import backtype.storm.spout.*;
+import backtype.storm.task.*;
+import backtype.storm.topology.*;
+import backtype.storm.topology.base.*;
+import backtype.storm.tuple.*;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
 
 // Each bolt/spout class in java is just a proxy for the C++ class equivalent
 public class SearchTopology {
 
-    public static class QuerySpout
-        extends ShellSpout implements IRichSpout {
-            public QuerySpout()
-            { super("/bin/sh", "run", "--spout=query"); }
-            @Override
-            public Map<String, Object> getComponentConfiguration()
-            { return null; }
-            @Override
-            public void declareOutputFields(OutputFieldsDeclarer declarer) {
-                // nodeID == graph node
-                declarer.declare(new Fields("requestID", "userID"));
+    // walk the graph, emit the neighbor vertices
+    public static class BFS extends BaseBasicBolt {
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            Object id = tuple.getValue(0);
+            String vertex = tuple.getString(1);
+            // XXX walk the graph
+            for (int i = 0; i < 2; i++) {
+                collector.emit(new Values(id, vertex + "+0"));
+                collector.emit(new Values(id, vertex + "+1"));
+                collector.emit(new Values(id, vertex + "+2"));
+                collector.emit(new Values(id, vertex + "+3"));
+                collector.emit(new Values(id, vertex + "+4"));
             }
         }
-
-    public static class BFSBolt
-        extends ShellBolt implements IRichBolt {
-            public BFSBolt()
-            { super("/bin/sh", "run", "--bolt=user"); }
-            @Override
-            public Map<String, Object> getComponentConfiguration()
-            { return null; }
-            @Override
-            public void declareOutputFields(OutputFieldsDeclarer declarer) {
-                declarer.declare(new Fields("requestID", "userID"));
-            }
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "vertex"));
+        }
     }
 
-    public static class UserBolt
-        extends ShellBolt implements IRichBolt {
-            public UserBolt()
-            { super("/bin/sh", "run", "--bolt=user"); }
-            @Override
-            public Map<String, Object> getComponentConfiguration()
-            { return null; }
-            @Override
-            public void declareOutputFields(OutputFieldsDeclarer declarer) {
-                declarer.declareStream("toFeature", // to FeatureBolt
-                        new Fields("requestID", "imageID"));
-                declarer.declareStream("toStats", // to ReqStatBolt
-                        new Fields("requestID", "userID", "numImages"));
-            }
+    // remove duplicate vertices
+    public static class PartialUniquer extends BaseBatchBolt {
+        BatchOutputCollector _collector;
+        Set<String> _vertices = new HashSet<String>();
+        Object _id;
+        @Override
+        public void prepare(Map conf, TopologyContext context,
+                BatchOutputCollector c, Object id) {
+            _collector = c;
+            _id = id;
         }
-
-    public static class FeatureBolt
-        extends ShellBolt implements IRichBolt {
-            public FeatureBolt()
-            { super("/bin/sh", "run", "--bolt=feature"); }
-            @Override
-            public Map<String, Object> getComponentConfiguration()
-            { return null; }
-            @Override
-            public void declareOutputFields(OutputFieldsDeclarer declarer)
-            { declarer.declare(new Fields("requestID", "imageID")); }
+        @Override
+        public void execute(Tuple tuple) {
+            _vertices.add(tuple.getString(1));
         }
-
-    public static class ReqStatBolt
-        extends ShellBolt implements IRichBolt {
-            public ReqStatBolt()
-            { super("/bin/sh", "run", "--bolt=reqstat"); }
-            @Override
-            public Map<String, Object> getComponentConfiguration()
-            { return null; }
-            @Override
-            public void declareOutputFields(OutputFieldsDeclarer declarer)
-            { }
+        @Override
+        public void finishBatch() {
+            for (String vertex : _vertices)
+                _collector.emit(new Values(_id, vertex));
         }
-
-    public static class MontageBolt
-        extends ShellBolt implements IRichBolt {
-            public MontageBolt()
-            { super("/bin/sh", "run", "--bolt=montage"); }
-            @Override
-            public Map<String, Object> getComponentConfiguration()
-            { return null; }
-            @Override
-            public void declareOutputFields(OutputFieldsDeclarer declarer)
-            { }
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "vertex"));
         }
+    }
 
-    public static class StitcherConfig {
-        public StitcherConfig() { }
-
-        public int spoutTasks, featureTasks;
-        public int userTasks, montageTasks, reqstatTasks;
-        public int maxTaskParallel, numWorkers, localSleep;
-
-        private void parseLine(String line)
-            throws IllegalArgumentException {
-            String[] cols = line.split(" ");
-            if (line.charAt(0) == '#')
-                return;
-            if (!cols[0].equals("storm"))
-                return;
-            if (cols.length != 3)
-                throw new IllegalArgumentException(
-                        "Invalid columns: " + line);
-            if (cols[1].equals("spout"))
-                spoutTasks = Integer.parseInt(cols[2]);
-            else if (cols[1].equals("feature"))
-                featureTasks = Integer.parseInt(cols[2]);
-            else if (cols[1].equals("user"))
-                userTasks = Integer.parseInt(cols[2]);
-            else if (cols[1].equals("montage"))
-                montageTasks = Integer.parseInt(cols[2]);
-            else if (cols[1].equals("reqstat"))
-                reqstatTasks = Integer.parseInt(cols[2]);
-            else if (cols[1].equals("maxparallel"))
-                maxTaskParallel = Integer.parseInt(cols[2]);
-            else if (cols[1].equals("localsleep"))
-                localSleep = Integer.parseInt(cols[2]);
-            else if (cols[1].equals("workers"))
-                numWorkers = Integer.parseInt(cols[2]);
-            else
-                throw new IllegalArgumentException(
-                        "Invalid identifier for storm: " + cols[1]);
+    // don't forward vertices that don't match some filter
+    public static class Filter extends BaseBasicBolt {
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            Object id = tuple.getValue(0);
+            String vertex = tuple.getString(1);
+            if (vertex.contains("+0"))
+                collector.emit(new Values(id, vertex));
         }
-        public void readConfig(String path)
-            throws IllegalArgumentException, FileNotFoundException, IOException
-        {
-            BufferedReader r;
-            String line;
-            r = new BufferedReader(new FileReader(path));
-            line = r.readLine();
-            while (null != line) {
-                parseLine(line);
-                line = r.readLine();
-            }
-            r.close();
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "vertex"));
         }
+    }
+
+    // for each vertex, emit the IDs of images associated with it
+    public static class ImageList extends BaseBasicBolt {
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            Object id = tuple.getValue(0);
+            String vertex = tuple.getString(1);
+            // XXX
+        }
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "image"));
+        }
+    }
+
+    // compute HOG operator; emit images that have objects in them;
+    // store the hog feature set in the object store
+    public static class Hog extends BaseBasicBolt {
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            Object id = tuple.getValue(0);
+            String vertex = tuple.getString(1);
+            // XXX
+        }
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "result"));
+        }
+    }
+
+    // smush the images together into one, return the key of the image in the
+    // object store
+    public static class Montage extends BaseBasicBolt {
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            Object id = tuple.getValue(0);
+            String vertex = tuple.getString(1);
+            collector.emit(new Values(id, "montagekeynigga"));
+        }
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "result"));
+        }
+    }
+
+    public static LinearDRPCTopologyBuilder construct() {
+        LinearDRPCTopologyBuilder builder =
+            new LinearDRPCTopologyBuilder("search");
+
+        builder.addBolt(new BFS(), 2);
+        builder.addBolt(new BFS(), 2).shuffleGrouping();
+        //builder.addBolt(new BFS(), 4).shuffleGrouping();
+        //builder.addBolt(new BFS(), 4).shuffleGrouping();
+        //builder.addBolt(new BFS(), 4).shuffleGrouping();
+        builder.addBolt(new PartialUniquer(), 4).fieldsGrouping(new Fields("id", "vertex"));
+        builder.addBolt(new Filter(), 3).shuffleGrouping();
+        //builder.addBolt(new ImageList(), 4).shuffleGrouping();
+        //builder.addBolt(new Hog(), 1).shuffleGrouping();
+
+        return builder;
     }
 
     public static void main(String[] args) throws Exception {
-        if (args.length < 1)
-            throw new IllegalArgumentException("Specify path to config file");
-
-        StitcherConfig sc = new StitcherConfig();
-        sc.readConfig(args[0]);
-
-        TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("spout", new QuerySpout(), sc.spoutTasks);
-        builder.setBolt("user", new UserBolt(), sc.userTasks)
-            .shuffleGrouping("spout");
-        builder.setBolt("feature", new FeatureBolt(), sc.featureTasks)
-            .shuffleGrouping("user", "toFeature");
-        builder.setBolt("montage", new MontageBolt(), sc.montageTasks)
-            .fieldsGrouping("feature", new Fields("requestID"));
-
-        builder.setBolt("reqstat", new ReqStatBolt(), sc.reqstatTasks)
-            .fieldsGrouping("user", "toStats", new Fields("requestID"));
+        LinearDRPCTopologyBuilder builder = construct();
 
         Config conf = new Config();
         conf.setDebug(true);
 
-        if (args.length > 1) {
-            conf.setNumWorkers(sc.numWorkers);
-            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
-        } else {
-            conf.setMaxTaskParallelism(sc.maxTaskParallel);
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("search", conf, builder.createTopology());
-            Thread.sleep(sc.localSleep * 1000); // ms
-            cluster.shutdown();
-        }
+        conf.setMaxTaskParallelism(1);
+        LocalDRPC drpc = new LocalDRPC();
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("search-drpc", conf,
+                builder.createLocalTopology(drpc));
+
+        // XXX add some tags to guide the filter
+        String vertex = new String("1337");
+        System.out.println("output: " + drpc.execute("search", vertex));
+        // XXX have some loop that generates queries
+
+        cluster.shutdown();
+        drpc.shutdown();
     }
 }
 
--- src/opencv/storm/makejar.sh
+++ src/opencv/storm/makejar.sh
@@ -9,24 +9,24 @@ JARS=/usr/local/src/storm/latest/lib/storm-core-0.9.3.jar
 
 [[ ! -e "$JARS" ]] && echo "storm jar not found" && exit 1
 
-killall -s 1 stormstitcher || true
+killall -s 1 stormfuncs || true
 
 echo Compiling ...
-javac -cp $JARS StitcherTopology.java
+javac -cp $JARS SearchTopology.java
 
-[[ ! -e ../stormstitcher ]] && \
-	echo "Compile stormstitcher first" && exit 1
+[[ ! -e ../stormfuncs ]] && \
+	echo "Compile stormfuncs first" && exit 1
 
 [[ ! -e ../pulse.conf ]] && \
 	echo "../pulse.conf missing" && exit 1
 
 [[ ! -d resources ]] && mkdir resources
 
-cp ../stormstitcher resources/
+cp ../stormfuncs resources/
 cp ../pulse.conf resources/
 
-echo Creating jar ...
-jar cf stitcher.jar Stitcher*.class resources/
+echo Creating jars ...
+jar cf search.jar Search*.class resources/
 
 rm -f *.class
 echo Done.
