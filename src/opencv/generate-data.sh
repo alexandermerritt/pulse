a392d65fc1bffc1fed7efd1ca846b064bdcab1e1
Alexander Merritt <merritt.alex@gatech.edu>
2015-02-19 15:03:03 -0500

    not using scons

--- src/opencv/generate-data.sh
+++ src/opencv/generate-data.sh
@@ -2,12 +2,11 @@
 set -e
 set -u
 
-exec=./load_egonet
-
-[[ ! -e $exec  ]] && scons -j6
+exec=load_egonet
+make DEBUG=no $exec
 
 # small dataset
-$exec proto ./inputs/ego_gplus_paths-small.in ./inputs/flickr_social_paths-small.in
+./$exec proto ./inputs/ego_gplus_paths-small.in ./inputs/flickr_social_paths-small.in
 mv -v graph.pb inputs/graph-small.pb
 mv -v imagelist.pb inputs/imagelist-small.pb
 mv -v graph-ids.txt inputs/
