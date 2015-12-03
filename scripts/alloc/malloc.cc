1ab36c3a1e6051633320f8cff6df717751b426ea
Alexander Merritt <merritt.alex@gatech.edu>
2015-12-02 21:01:30 -0500

    malloc: cleaned up, use of lambda funcs for input

--- scripts/alloc/malloc.cc
+++ scripts/alloc/malloc.cc
@@ -1,11 +1,9 @@
 #include <fstream>
 #include <random>
-#include <stack>
 #include <sstream>
-#include <bitset>
 #include <iostream>
 #include <array>
-#include <deque>
+#include <functional>
 
 #include <cassert>
 #include <cstddef>
@@ -81,94 +79,20 @@ void *map_remap(void* oldp, size_t oldb, size_t newb)
     return newp;
 }
 
-int testinput(int narg, char *args[])
-{
-    string line;
-    struct timespec clk1, clk2;
-
-    if (narg != 2)
-        return 1;
-
-    size_t total_sz(0);
-    size_t max_vals, nvals(0);
-
-    size_t locs_sz(0);
-    loc *locs(nullptr);
-
-    size_t len, accum(0);
-
-    for (int i = 0; i < 2; i++) {
-        if (i == 1) {
-            locs_sz = max_vals * sizeof(loc);
-            assert(locs_sz > 0);
-            locs = (loc*)map_alloc(locs_sz);
-            assert(locs);
-        }
-        string input(args[1]);
-        ifstream ifs(input);
-        while (getline(ifs, line)) {
-            nvals++;
-            if (i == 0)
-                continue;
-            stringstream ss(line);
-            ss >> len;
-            total_sz += len;
-            locs[nvals-1].len = len;
-        }
-        ifs.close();
-        if (i == 0)
-            max_vals = nvals;
-        nvals = 0;
-    }
-
-    long every = max_vals / 10.;
-    clock_gettime(CLOCK_REALTIME, &clk1);
-    for (nvals = 0; nvals < max_vals; nvals++) {
-        len = locs[nvals].len;
-        locs[nvals].addr = (uintptr_t)malloc(len);
-        assert(locs[nvals].addr);
-        memset((void*)locs[nvals].addr, 0, len);
-        if (nvals % every == 0) {
-            clear_line();
-            printf("# %2.1f %%", 100.f * accum / total_sz);
-            fflush(stdout);
-        }
-        if ((accum += locs[nvals].len) > total_sz)
-            break;
-    }
-    clock_gettime(CLOCK_REALTIME, &clk2);
-    printf("\n");
-
-    long d = (clk2.tv_sec * 1e9 + clk2.tv_nsec)
-        - (clk1.tv_sec * 1e9 + clk1.tv_nsec);
-
-    long rss = get_rss(), actual = rss-locs_sz;
-    cout << "total rss overhead msec" << endl;
-    cout << total_sz;
-    cout << " " << actual;
-    cout << " " << (static_cast<float>(actual)/total_sz);
-    cout << " " << (float)d/1e6;
-    cout << endl;
-
-    return 0;
-}
-
 class LiveSet
 {
     public:
-
-        LiveSet(long bytes) : max_live(bytes), live(0L),
-                            last_idx(0), drop_misses(0),
-                            locs(nullptr), sizes(nullptr),
-                            fltop(0L),
-                            gen(rd()), dist(0, nlocs-1) {
+        LiveSet(long bytes)
+            : max_live(bytes), live(0L),
+            last_idx(0), drop_misses(0),
+            locs(nullptr), sizes(nullptr),
+            fltop(0L), gen(rd()), dropRand(0, nlocs-1) {
             if (bytes < 1)
                 throw runtime_error("LiveSet with empty set?");
             locs = (uintptr_t*)map_alloc(nlocs*sizeof(*locs));
             sizes = (uint32_t*)map_alloc(nlocs*sizeof(*sizes));
             assert( locs && sizes );
             freelist.fill(-1);
-            fltop = 0L;
         }
 
         ~LiveSet() {
@@ -177,31 +101,14 @@ class LiveSet
             munmap(sizes, nlocs*sizeof(*sizes));
         }
 
-        void fillFixed(uint32_t objsize) {
-            if ((nlocs * objsize) < max_live)
-                throw runtime_error("resize needed");
-            for (long n = 0; n < nlocs; n++) {
-                assert( locs[n] = (uintptr_t)malloc(objsize) );
-                sizes[n] = objsize;
-                live += objsize;
-                nobjs++;
-                if (live >= max_live) {
-                    last_idx = n;
-                    break;
-                }
-            }
-            freelist.fill(-1);
-            fltop = 0L;
-        }
+        typedef function<long (void)> numGen_f;
 
-        // add new objects. if we exceed our allowance, delete random
-        // objects until we are within allowance again.
-        void injectFixed(long n, uint32_t objsize) {
-            off_t idx;
-            if (n == 0)
+        void injectValues(long injectSz, numGen_f genf) {
+            off_t idx, nn(0L), objsize, total(injectSz);
+            if (injectSz < 1)
                 return;
-            drop(objsize<<2);
-            for (long nn = 0; nn < n; nn++) {
+            drop(genf()<<2);
+            while (injectSz > 0) {
                 if (fltop == 0) {
                     if ((idx = ++last_idx) >= nlocs)
                         throw runtime_error("Exceeded nlocs");
@@ -209,28 +116,31 @@ class LiveSet
                     idx = freelist[--fltop];
                     assert( idx > -1 );
                 }
+                objsize = genf();
                 assert( !locs[idx] );
                 assert( locs[idx] = (uintptr_t)malloc(objsize) );
                 sizes[idx] = objsize;
                 live += objsize;
+                injectSz -= objsize;
                 nobjs++;
-                // print progress
-                if ((nn%100000L)==0) {
-                    clear_line(); printf("# %3.3lf %%  ",
-                        (float)nn*100/n); fflush(stdout);
-                }
                 drop(objsize<<2);
+                // print progress
+                if (!(nn++%100000L)) {
+                    clear_line(); printf("# %3.3lf %%  ",
+                        100.*(float)(total-injectSz)/total);
+                    fflush(stdout);
+                }
             }
             printf("\n");
         }
 
-        // free objects at random until we are below max
+        // free objects at random until we are below threshold
         void drop(off_t atleast = 0UL) {
             if (live <= (max_live - atleast))
                 return;
             long idx;
             while (live > (max_live - atleast)) {
-                idx = dist(gen) % last_idx; // should hit most of the time
+                idx = dropRand(gen) % last_idx; // should hit most of the time
                 if (locs[idx]) {
                     free((void*)locs[idx]);
                     live -= sizes[idx];
@@ -245,13 +155,6 @@ class LiveSet
             }
         }
 
-        void addDist(long mean, long std) {
-            // TODO
-            // random_device rd;
-            // mt19937 gen(rd());
-            // uniform_int_distribution<long> d(mean, std);
-        }
-
         void freeAll() {
             for (long n = 0; n < nlocs; n++) {
                 if (locs[n]) {
@@ -259,9 +162,11 @@ class LiveSet
                     locs[n] = sizes[n] = 0;
                 }
             }
-            live = nobjs = 0L;
             freelist.fill(-1);
+            live = 0L;
+            nobjs = 0L;
             fltop = 0L;
+            last_idx = 0L;
         }
 
         static inline size_t ceil4K(size_t val) {
@@ -276,10 +181,8 @@ class LiveSet
                 + ceil4K(sizeof(LiveSet));
         }
 
-        long get_nobjs(void) { return nobjs; }
-        long get_live(void)  { return live; }
+        // Class variables
 
-    private:
         long max_live, live;    // working set max and current, bytes
         long nobjs;             // pointers allocated
         long last_idx, drop_misses;
@@ -288,59 +191,117 @@ class LiveSet
         uintptr_t *locs;        // array with nlocs items
         uint32_t *sizes;        // array with nlocs items
 
+    private:
         constexpr static off_t flmax = nlocs>>2;
         array<off_t, flmax> freelist;
         off_t fltop;
 
         random_device rd;
         mt19937 gen;
-        uniform_int_distribution<long> dist;
+        uniform_int_distribution<long> dropRand;
 
         LiveSet();
 };
 
-// modeled after synthetic workload from: Rumble et al. FAST'14
-int testlive(int narg, char *args[])
+//
+// Workloads from FAST'14 paper.
+//
+auto W1before = [] () { return 100L; };
+auto W2before = W1before;
+auto W3before = W1before;
+auto W4before = [] () {
+    const long m1 = 100, m2 = 150;
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<long> d(m1, m2);
+    return d(gen);
+};
+auto W5before = W4before;
+auto W6before = [] () {
+    const long m1 = 100, m2 = 250;
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<long> d(m1, m2);
+    return d(gen);
+};
+auto W7before = [] () {
+    const long m1 = 1000, m2 = 2000;
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<long> d(m1, m2);
+    return d(gen);
+};
+auto W8before = [] () {
+    const long m1 = 50, m2 = 150;
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<long> d(m1, m2);
+    return d(gen);
+};
+
+auto W2after  = [] () { return 130L; };
+auto W3after  = W2after;
+auto W4after  = [] () {
+    const long m1 = 200, m2 = 250;
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<long> d(m1, m2);
+    return d(gen);
+};
+auto W5after  = W4after;
+auto W6after  = W7before;
+auto W7after  = [] () {
+    const long m1 = 1500, m2 = 2500;
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<long> d(m1, m2);
+    return d(gen);
+};
+auto W8after  = [] () {
+    const long m1 = 5000, m2 = 15000;
+    static random_device rd;
+    static mt19937 gen(rd());
+    static uniform_int_distribution<long> d(m1, m2);
+    return d(gen);
+};
+
+void dumpstats(const char *name, LiveSet *liveset)
 {
-    float rss, eff;
+    float rss = get_rss() - liveset->overhead();
+    float eff = rss/liveset->live;
+    printf("%16s %16s %12.3f %12.5f\n",
+            name, "W1", rss/MB, eff);
+}
 
-    LiveSet *liveset(nullptr);
-    const long live = 1L<<30;
-    const long wss = 1L<<32;
-    const uint32_t objsize = 100, objsize2 = 130;
-    const float dropPct = 90;
-
+// modeled after synthetic workload from: Rumble et al. FAST'14
+int testlive(const char *name, long livewss, long injectwss)
+{
     // must allocate liveset on heap because freelist array is huge
-    assert( liveset = new LiveSet(live) );
-
-    // FIXME how to subtract out ELF segments, globals, SO objects?
-    // supermalloc performs a huge heap alloc during lib init
+    LiveSet *liveset(nullptr);
+    assert( liveset = new LiveSet(livewss) );
+    printf("# size of LiveSet: %lf MiB\n",
+            (float)sizeof(LiveSet)/(1<<20));
+    printf("# overhead of LiveSet: %lf MiB\n",
+            (float)liveset->overhead()/(1<<20));
 
     printf("cmd testname-sz rss-MB eff\n");
 
-    cout << "# injectFixed " << objsize << endl;
-    liveset->injectFixed(wss/objsize, objsize);
-    rss = get_rss() - liveset->overhead();
-    eff = rss/liveset->get_live();
-    printf("%16s %16s-%04d %12.3f %12.5f\n",
-            args[0], "injectFixed", objsize, rss/MB, eff);
+    liveset->injectValues(injectwss, W2before);
+    liveset->injectValues(injectwss, W2after);
+    dumpstats(name, liveset);
+    liveset->freeAll();
 
-    cout << "# drop " << (live*dropPct/100./(1<<20)) << " MiB" << endl;
-    liveset->drop( live * (dropPct/100) );
-    rss = get_rss() - liveset->overhead();
-    eff = rss/liveset->get_live();
-    printf("%16s %16s-%04d %12.3f %12.5f\n",
-            args[0], "drop", (int)dropPct, rss/MB, eff);
+    liveset->injectValues(injectwss, W2before);
+    liveset->injectValues(injectwss, W2after);
+    dumpstats(name, liveset);
+    liveset->freeAll();
 
-    cout << "# injectFixed " << objsize2 << endl;
-    liveset->injectFixed(wss/objsize2, objsize2);
-    rss = get_rss() - liveset->overhead();
-    eff = rss/liveset->get_live();
-    printf("%16s %16s-%04d %12.3f %12.5f\n",
-            args[0], "injectFixed", objsize2, rss/MB, eff);
+    liveset->injectValues(injectwss, W2before);
+    liveset->injectValues(injectwss, W2after);
+    dumpstats(name, liveset);
+    liveset->freeAll();
 
     delete liveset;
-
     return 0;
 }
 
@@ -350,7 +311,7 @@ int main(int narg, char *args[])
         perror("mlockall");
         return 1;
     }
-    //return testinput(narg, args);
-    //return testdist(narg, args);
-    return testlive(narg, args);
+    long live_wss = 1UL<<29;
+    long inject_wss = 1UL<<31;
+    return testlive(args[0], live_wss, inject_wss);
 }
