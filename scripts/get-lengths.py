e7508727661247d075d040b13556e0d62a45d7ec
Alexander Merritt <merritt.alex@gatech.edu>
2015-12-18 17:17:14 -0500

    protect print from clobbering

--- scripts/get-lengths.py
+++ scripts/get-lengths.py
@@ -2,7 +2,24 @@
 import sys
 import time
 import threading
+import queue
 import urllib.request as ur # >= python3
+
+NTHREADS = 32
+
+######################################################################
+
+pq = queue.Queue()
+
+def printq(q):
+    while True:
+        while not q.empty():
+            print( q.get() )
+            sys.stdout.flush()
+        time.sleep(2)
+        if q.empty():
+            print('exiting')
+            return
 
 def gethead(urlstr):
     head = ''
@@ -21,12 +38,13 @@ def getlen(urlstr):
             break
     return l
 
-def printlen(urls):
+def printlen(urls,q):
     for url in urls:
-        print(url + ' ' + str(getlen(str(url))))
+        q.put(url + ' ' + str(getlen(str(url))))
         time.sleep(0.5)
 
 def process(urls,nthreads):
+    global pq
     per = int(len(urls) / nthreads)
     while len(urls) > 0:
         s = set()
@@ -34,9 +52,12 @@ def process(urls,nthreads):
             s.add(urls.pop())
             if len(urls) == 0:
                 break
-        t = threading.Thread(target=printlen, args=(s,))
+        t = threading.Thread(target=printlen, args=(s,pq,))
         t.daemon = False
         t.start()
+    t = threading.Thread(target=printq, args=(pq,))
+    t.daemon = False
+    t.start()
 
 def readurls():
     urls = set()
@@ -51,5 +72,5 @@ def readurls():
         raise Exception('Empty set of URLs')
     return urls
 
-process( readurls(), 16 )
+process( readurls(), NTHREADS )
 
